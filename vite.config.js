188692d7b9228869d4ba093dc52ae326fd728c71
Hmtgit7 <hmtloharcoding3579@gmail.com>
2025-04-11 13:21:52 +0530

    update vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import path from 'path';
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(new URL('.', import.meta.url).pathname, 'src') }],
-  },
-  plugins: [reactRefresh()],
-});
+  plugins: [reactRefresh()]
+})
